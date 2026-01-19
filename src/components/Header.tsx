37966cc901e615ed25f03154873046bf29d0ba33
Wix Remote Machine <support@wix.com>
2026-01-19 13:36:59 +0000

    Mover imagem ao lado do menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,7 +41,7 @@ export default function Header() {
       <div className="max-w-[120rem] mx-auto px-6 lg:px-12 py-4">
         <div className="flex items-center justify-between gap-6">
           {/* Logo */}
-          <Link to="/" className="flex items-center justify-center mx-auto lg:mx-0">
+          <Link to="/" className="flex items-center justify-center lg:mx-0">
             <Image 
               src="https://static.wixstatic.com/media/904ff8_e139363c186d4f4f8a5fd7c11421908a~mv2.png" 
               width={200}
@@ -120,10 +120,10 @@ export default function Header() {
             </Link>
           </nav>
 
-          {/* Mobile Menu Button - Positioned at far right */}
+          {/* Mobile Menu Button */}
           <button
             onClick={() => setIsMenuOpen(!isMenuOpen)}
-            className="lg:hidden text-foreground p-2 ml-auto"
+            className="lg:hidden text-foreground p-2"
             aria-label="Alternar menu"
           >
             {isMenuOpen ? <X size={28} /> : <Menu size={28} />}
