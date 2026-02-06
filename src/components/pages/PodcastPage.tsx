5447e03e072da33cb65d9eeb5523a1297c73e611
Wix Remote Machine <support@wix.com>
2026-02-06 07:31:20 +0000

    Published

--- src/components/pages/PodcastPage.tsx
+++ src/components/pages/PodcastPage.tsx
@@ -71,9 +71,7 @@ export default function PodcastPage() {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.5, duration: 0.6 }}
-            >
-              Empresário e Conselho em Diálogo
-            </motion.p>
+            >Empresário, C-Levels e Conselho em Diálogo</motion.p>
 
             {/* Description */}
             <motion.p
